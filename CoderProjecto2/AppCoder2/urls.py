55dbc12bd57ae42e3044a32aa4ef1ff7ad9a7cf3
Alejandro <alopereyra79@gmail.com>
2022-10-12 00:06:30 -0300

    agrego templates con html

--- CoderProjecto2/AppCoder2/urls.py
+++ CoderProjecto2/AppCoder2/urls.py
@@ -13,11 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+
+from django.urls import path
 from AppCoder2.views import mostrar_inicio
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("AppCoder2/", include("AppCoder2.urls"))
-]
+    path('inicio/', mostrar_inicio),
+
+ ]
--- CoderProjecto2/AppCoder2/views.py
+++ CoderProjecto2/AppCoder2/views.py
@@ -5,4 +5,21 @@ from django.http import HttpResponse
 # Create your views here.
 
 def mostrar_inicio(request):
-    return HttpResponse("Hola mundo!!")
+    return render(request, "AppCoder2/inicio.html",)
+
+
+def cursos(request):
+    return render(request, "AppCoder2/cursos.html")
+
+
+def profesores(request):
+    return render(request, "AppCoder2/profesores.html")
+
+
+def estudiantes(request):
+    return render(request, "AppCoder2/estudiantes.html")
+
+
+def entregables(request):
+    return render(request, "AppCoder2/entregables.html")
+
